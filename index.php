6b73ddaa03713492bf62c3543bc3d16ab68fe9c1
zaeemul <zaeemul.haque@gmail.com>
2013-09-15 17:05:41 -0700

    chnages in files

--- index.php
+++ index.php
@@ -1,8 +1,50 @@
-<HTML>
-<HEAD>
-<TITLE> Hello World in PHP </TITLE>
-</HEAD>
-<BODY>
- <?php Echo "Hello World -----------Test with php"; ?>
-</BODY>
-</HTML>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="generator" content="CoffeeCup HTML Editor (www.coffeecup.com)">
+    <meta name="dcterms.created" content="Sun, 15 Sep 2013 01:03:30 GMT">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <title>first</title>
+    
+    <link href="styles.css" rel="stylesheet" type="text/css" />
+    <!--[if IE]>
+    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+  <div id="header">
+  
+<h1>Welcome to My Web Page for Dynamic Web Development </h1>
+<p>
+    <h2>zaeemul.biz</h2>
+<p>
+	<div id="banner"></div>
+	
+    <table>
+  <thead>
+    <tr>
+      <th class="from">My Name</th>
+      <th class="subject">My Biography</th>
+      <th class="date">Editors that I like</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>Zaeemul Haque</td>
+      <td>I am taking this course as elective as previously I took web development, I felt that I should take some
+kind of challenging Web developing courses so I decided that I take this one. I am pursuing MS in Information Management 
+
+Systems.<br>I got my Bachelor's degree from the University of Houston and Diploma in Database Development and Administratio from 
+
+Herzing College. I am working as a Database Administration at Automatic Data Process in Toronto.</td>
+      <td>Sublime,CoffeeCup,Eclipse</td>
+    </tr>
+  </tbody>
+</table>
+	<div id="footer">
+	<p id="legal">This is my First Project</p>
+</div>
+  </body>
+</html>
